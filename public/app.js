b4a478d6f4802c1d028558d323138952461546aa
Sean Joesting <seanmarcusjoesting+1@gmail.com>
2022-06-27 11:55:25 -0400

    testing new SSL

--- public/app.js
+++ public/app.js
@@ -1,6 +1,59 @@
-document.addEventListener("DOMContentLoaded", function() {
+
+/*NOTE:
+These 4 functions are used to auto format the phone number input. 
+
+
+*/
+const isNumericInput = (event) => {
+    const key = event.keyCode;
+    return ((key >= 48 && key <= 57) || // Allow number line
+        (key >= 96 && key <= 105) // Allow number pad
+    );
+};
+
+const isModifierKey = (event) => {
+    const key = event.keyCode;
+    return (event.shiftKey === true || key === 35 || key === 36) || // Allow Shift, Home, End
+        (key === 8 || key === 9 || key === 13 || key === 46) || // Allow Backspace, Tab, Enter, Delete
+        (key > 36 && key < 41) || // Allow left, up, right, down
+        (
+            // Allow Ctrl/Command + A,C,V,X,Z
+            (event.ctrlKey === true || event.metaKey === true) &&
+            (key === 65 || key === 67 || key === 86 || key === 88 || key === 90)
+        )
+};
+
+const enforceFormat = (event) => {
+    // Input must be of a valid number format or a modifier key, and not longer than ten digits
+    if(!isNumericInput(event) && !isModifierKey(event)){
+        event.preventDefault();
+    }
+};
+
+const formatToPhone = (event) => {
+    if(isModifierKey(event)) {return;}
+
+    const input = event.target.value.replace(/\D/g,'').substring(0,10); // First ten digits of input only
+    const areaCode = input.substring(0,3);
+    const middle = input.substring(3,6);
+    const last = input.substring(6,10);
+
+    if(input.length > 6){event.target.value = `(${areaCode}) ${middle} - ${last}`;}
+    else if(input.length > 3){event.target.value = `(${areaCode}) ${middle}`;}
+    else if(input.length > 0){event.target.value = `(${areaCode}`;}
+};
+const API_URI = "https://api.eyerate.tech";
+const BUCKET_URL = "http://eyerate-widget-chat.s3-website.us-east-2.amazonaws.com";
+document.addEventListener("DOMContentLoaded", async function() {
     var me = document.querySelector('script[data-id][data-name="EyeRate"]');
-    var id = me.getAttribute('data-id');
+    var id = me.getAttribute('data-id');    
+
+    // eslint-disable-next-line no-undef
+    const settings = await (await fetch(`${BUCKET_URL}/settings/${id}/settings.json`)).json();
+    /*NOTE:
+    We start off by making an API call to get the settings for the corresponding widget. This includes color and text options for them.
+    */
+    const color = settings?.color || '#36ADAD';
     var chatButton = document.createElement('div');
     chatButton.style.position = 'fixed';
     chatButton.style.bottom = '20px';
@@ -8,7 +61,7 @@ document.addEventListener("DOMContentLoaded", function() {
     chatButton.style.width = '75px';
     chatButton.style.height = '75px';
     chatButton.style.borderRadius = '50%';
-    chatButton.style.backgroundColor = '#36ADAD';
+    chatButton.style.backgroundColor = color;
     chatButton.style.cursor = 'pointer';
     chatButton.style.boxShadow = '0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)';
     chatButton.style.transition = 'all 0.2s ease-in-out';
@@ -51,7 +104,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var chatBoxHeader = document.createElement('div');
     chatBoxHeader.style.width = '100%';
     chatBoxHeader.style.height = '50px';
-    chatBoxHeader.style.backgroundColor = '#36ADAD';
+    chatBoxHeader.style.backgroundColor = color;
     chatBoxHeader.style.borderTopLeftRadius = '5px';
     chatBoxHeader.style.borderTopRightRadius = '5px';
     chatBoxHeader.style.textAlign = 'center';
@@ -93,18 +146,21 @@ document.addEventListener("DOMContentLoaded", function() {
     nameInput.style.height = '30px';
     nameInput.style.marginBottom = '10px';
     nameInput.style.borderRadius = '5px';
-    nameInput.style.border = '1px solid #36ADAD';
+    nameInput.style.border = `1px solid ${color}`;
     chatBubbleForm.appendChild(nameInput);
 
     var phoneInput = document.createElement('input');
     phoneInput.type = 'text';
-    phoneInput.placeholder = 'Phone Number';
+    phoneInput.placeholder = 'Mobile Number';
     phoneInput.style.width = '100%';
     phoneInput.style.height = '30px';
     phoneInput.style.marginBottom = '10px';
     phoneInput.style.borderRadius = '5px';
-    phoneInput.style.border = '1px solid #36ADAD';
+    phoneInput.style.border = `1px solid ${color}`;
+    phoneInput.addEventListener('keydown', enforceFormat);
+    phoneInput.addEventListener('keyup', formatToPhone);
     chatBubbleForm.appendChild(phoneInput);
+
 
     var messageInput = document.createElement('textarea');
     messageInput.placeholder = 'Message...';
@@ -112,7 +168,7 @@ document.addEventListener("DOMContentLoaded", function() {
     messageInput.style.height = '100px';
     messageInput.style.marginBottom = '10px';
     messageInput.style.borderRadius = '5px';
-    messageInput.style.border = '1px solid #36ADAD';
+    messageInput.style.border = `1px solid ${color}`;
     messageInput.style.padding = '4px';
     chatBubbleForm.appendChild(messageInput);
 
@@ -121,19 +177,20 @@ document.addEventListener("DOMContentLoaded", function() {
     submitButton.style.width = '100%';
     submitButton.style.height = '30px';
     submitButton.style.borderRadius = '5px';
-    submitButton.style.border = '1px solid #36ADAD';
-    submitButton.style.backgroundColor = '#36ADAD';
-    submitButton.style.color = 'white';
+    submitButton.style.border = `1px solid ${color}`;
+    submitButton.style.backgroundColor = 'white';
+    submitButton.style.color = color;
     submitButton.style.cursor = 'pointer';
     submitButton.style.transition = 'all 0.2s ease-in-out';
     submitButton.onmouseover = function() {
-    submitButton.style.backgroundColor = 'white';
-    submitButton.style.color = '#36ADAD';
+    submitButton.style.backgroundColor = color;
+    submitButton.style.color = 'white';
 
     };
     submitButton.onmouseout = function() {
-    submitButton.style.backgroundColor = '#36ADAD';
-    submitButton.style.color = 'white';
+    submitButton.style.backgroundColor = 'white';
+    submitButton.style.color = color;
+    submitButton.backgroundColor.color = 'white';
     };
     chatBubbleForm.appendChild(submitButton);
 
@@ -162,9 +219,10 @@ document.addEventListener("DOMContentLoaded", function() {
     submitButton.onclick = async function() {
         const params = {
             content: messageInput.value,
-            phone: phoneInput.value,
+            phone: `+1${phoneInput.value.replaceAll('(','').replaceAll(')','').replaceAll('-','').replaceAll(' ','')}`,
             customerName: nameInput.value,
-            id,
+            id: settings?.location,
+            email: settings?.email,
         };
             
             
@@ -178,7 +236,7 @@ document.addEventListener("DOMContentLoaded", function() {
             body: JSON.stringify(params),
             };
         // eslint-disable-next-line no-undef
-        fetch('http://localhost:8081/v1/public/widget/sendMessage', options )
+        fetch('http://localhost:8081/v1/public/widget/chat/sendMessage', options )
         .then(res => {
             nameInput.style.display = 'none';
             phoneInput.style.display = 'none';
@@ -187,14 +245,22 @@ document.addEventListener("DOMContentLoaded", function() {
             chatBubble.style.display = 'none';
             disclaimer.style.display = 'none';
             var successIcon = document.createElement('img');
-            successIcon.src = 'https://icons.veryicon.com/png/o/miscellaneous/new-version-of-star-selected-icon/success-26.png';
-            successIcon.style.width = '30px';
-            successIcon.style.height = '30px';
+            successIcon.src = 'https://cdn3.iconfinder.com/data/icons/basicolor-arrows-checks/24/149_check_ok-512.png';
+            successIcon.style.width = '60px';
+            successIcon.style.height = '60px';
             successIcon.style.position = 'absolute';
-            successIcon.style.top = '50%';
+            successIcon.style.bottom = '20%';
             successIcon.style.left = '50%';
             successIcon.style.transform = 'translate(-50%, -50%)';
+            var successMessage = document.createElement('a');
+            successMessage.innerHTML = settings?.success_message?.text || "Message sent successfully! We will get back to you shortly via SMS.";
+            successMessage.href = settings?.success_message?.link;
+            successMessage.style.fontSize = "14px";
+            successMessage.style.color = color;
+            successMessage.target = "_blank";
+            successMessage.style.textAlign = "center";
             chatBubbleForm.appendChild(successIcon);
+            chatBubbleForm.appendChild(successMessage);
         })
         .catch(err => {
             console.log(err);
@@ -204,22 +270,22 @@ document.addEventListener("DOMContentLoaded", function() {
             submitButton.style.display = 'none';
             chatBubble.style.display = 'none';
             disclaimer.style.display = 'none';
-            var successIcon = document.createElement('img');
-            successIcon.src = 'https://icons.veryicon.com/png/o/miscellaneous/new-version-of-star-selected-icon/success-26.png';
-            successIcon.style.width = '80px';
-            successIcon.style.height = '80px';
-            successIcon.style.position = 'absolute';
-            successIcon.style.top = '50%';
-            successIcon.style.left = '50%';
-            successIcon.style.transform = 'translate(-50%, -50%)';
-            chatBubbleForm.appendChild(successIcon);
+            var errorIcon = document.createElement('img');
+            errorIcon.src = 'https://cdn0.iconfinder.com/data/icons/shift-interfaces/32/Error-512.png';
+            errorIcon.style.width = "50px";
+            errorIcon.style.height = "50px";
+            errorIcon.style.position = "absolute";
+            errorIcon.style.top = "50%";
+            errorIcon.style.left = "50%";
+            errorIcon.style.transform = 'translate(-50%, -50%)';
+            chatBubbleForm.appendChild(errorIcon);
         })
 
     };
     var chatBoxFooter = document.createElement('div');
     chatBoxFooter.style.width = '100%';
     chatBoxFooter.style.height = '40px';
-    chatBoxFooter.style.backgroundColor = '#36ADAD';
+    chatBoxFooter.style.backgroundColor = color;
     chatBoxFooter.style.borderBottomLeftRadius = '5px';
     chatBoxFooter.style.borderBottomRightRadius = '5px';
     chatBoxFooter.style.textAlign = 'center';
@@ -230,10 +296,11 @@ document.addEventListener("DOMContentLoaded", function() {
     chatBoxFooter.style.position = 'absolute';
     chatBoxFooter.style.bottom = '0px';
     var footerLogo = document.createElement('img');
-    footerLogo.src = 'https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/wp7tiawrmvcj5swxlczc';
+    footerLogo.src = settings?.logo || 'https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/wp7tiawrmvcj5swxlczc';
     footerLogo.style.position = 'absolute';
     footerLogo.style.top = '50%';
     footerLogo.style.left = '50%';
+    footerLogo.style.borderRadius = "50%";
     footerLogo.style.transform = 'translate(-50%, -50%)';
     chatBoxFooter.appendChild(footerLogo);
     /* Put the footer logo on the left and make it object cover */
@@ -243,12 +310,15 @@ document.addEventListener("DOMContentLoaded", function() {
     footerLogo.style.width = '30px';
     footerLogo.style.height = '30px';
     var disclaimer = document.createElement('div');
-    disclaimer.innerHTML = 'By submitting, you authorize EyeRate to send text messages with offers & other information, possibly using automated technology, to the number you provided. Message/data rates apply. Consent is not a condition of purchase.';
+    disclaimer.innerHTML = `By submitting, you authorize ${settings?.disclaimer_name} to send text messages with offers & other information, possibly using automated technology, to the number you provided. Message/data rates apply. Consent is not a condition of purchase.`;
     disclaimer.style.position = 'absolute';
     disclaimer.style.bottom = '50px';
     disclaimer.style.textAlign = 'center';
     disclaimer.style.padding = '10px';
     disclaimer.style.fontSize = '10px';
     disclaimer.style.color = 'black';
+    var publicLink = document.createElement('div');
+    publicLink.innerHTML = "Public Profile";
     chatBox.appendChild(disclaimer);
+    // chatBox.append(publicLink);
 });
--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App" style={{fontSize:8, width:'100vw',height:'100vh',display:'flex', justifyContent:'center', alignItems:'center'}}>
-     LETS GET MONEY
+     
     </div>
   );
 }
